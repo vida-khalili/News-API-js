8aa41b750f1a52e2ba22c5638fd21ca145030016
vida-khalili <98278710+vida-khalili@users.noreply.github.com>
2023-07-17 20:46:31 +0330

    style.css and script.js
    using bootstrap for style.
    fetch top headlines api in script.js

--- script/script.js
+++ script/script.js
@@ -6,9 +6,30 @@ const fetchTopHeadlineApi = async () => {
       `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
     );
     let result = await response.json();
-    console.log(result);
     return result;
   } catch (error) {
     console.log("error", error);
   }
 };
+
+const generateHeadlineList = async () => {
+  let headlines = await fetchTopHeadlineApi();
+  let articles = headlines.articles;
+  let topHeadLinesElement = document.querySelector(".top-headlines-part");
+  let headLineItem = `<ul></ul>`;
+  if (articles.length > 10) {
+    articles.length = 10;
+    articles.map((item) => {
+      console.log(item);
+      headLineItem = headLineItem + `<li>${item.source.name}</li>`;
+    });
+  } else {
+    articles.map((item) => {
+      console.log(item);
+    });
+  }
+  headLineItem = headLineItem + `</div>`;
+  // topHeadLinesElement.innerHTML = headLineItem;
+};
+
+generateHeadlineList();
